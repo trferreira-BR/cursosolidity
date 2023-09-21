5ab3380c98dee0e8b86bf3f1fad99181d90d5c18
Thiago Rafael Ferreira <34104414+trferreira-BR@users.noreply.github.com>
2023-09-21 20:02:50 -0300

    Update ExercicioCursoBlockchainTcc.sol

--- ExercicioCursoBlockchainTcc.sol
+++ ExercicioCursoBlockchainTcc.sol
@@ -1,1 +1,65 @@
+/*
+SPDX-License-Identifier: CC-BY-4.0
+(c) Desenvolvido por Thiago R. Ferreira
+This work is licensed under a Creative Commons Attribution 4.0 International License.
+*/
 
+pragma solidity 0.8.19;
+
+import "https://github.com/jeffprestes/cursosolidity/blob/master/bradesco_token_aberto.sol";
+
+// CONTRACT: 0x523490C78E5c100b7c845578eFA7C6E4817e3Fd4
+
+contract ExercicioCursoBlockchainTcc {
+
+    Cliente cliente;
+    ExercicioToken exercicioToken;
+
+    struct Cliente {
+        string primeiroNome;
+        string sobreNome;
+        address payable endereco; //0x0
+        bytes32 hashConta; // 0x0        
+        bool existe; //false
+    }
+
+    constructor(
+        string memory _primeiroNome,
+        string memory _sobreNome,        
+        string memory _agencia,
+        string memory _conta,
+        address _enderecoContratoToken) {
+
+        string memory strTemp = string.concat(_agencia, _conta);
+        bytes memory bTemp = bytes(strTemp);
+        bytes32 hashTemp = keccak256(bTemp);
+
+        Custodia custodiaTemp = new Custodia(hashTemp);
+        cliente = Cliente(_primeiroNome, _sobreNome, payable(address(custodiaTemp)), hashTemp, true);
+
+        exercicioToken = ExercicioToken(_enderecoContratoToken);
+    }
+
+    function meuSaldo() public view returns(uint256) {
+        return exercicioToken.balanceOf(address(this));
+    }
+
+    function gerarTokenParaEuCliente(uint256 _amount) public returns (bool){
+        return exercicioToken.mint(address(this), _amount);
+    }
+}
+
+contract Custodia {
+    bytes32 public hashConta;
+
+    event EtherRecebido();
+
+    constructor(bytes32 _hashConta) {
+        hashConta = _hashConta;
+    }
+
+    receive() external payable {
+        emit EtherRecebido();
+    }
+    
+}
